8d072f5516086ffb01eed15114e5fea504e4617b
Michal Chruslicki <mike@MacBook-Pro-Michal.local>
2017-10-11 01:00:40 +0200

    work in progress

--- js/app.js
+++ js/app.js
@@ -47,4 +47,17 @@ $(function() {
         });
     });
 
+
+    //karuzela 
+
+
+    $('.allSlides').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+
+
 });
